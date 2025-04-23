af89f143405657c47ed7c2b66ef21f6d96d00538
Mynameisjohndev <joaoantonio15161516@gmail.com>
2025-04-23 19:46:37 -0300

    feat: downgraded project to 0.78.2 stable version

--- App.tsx
+++ App.tsx
@@ -1,16 +1,20 @@
 import {NavigationContainer} from '@react-navigation/native';
 import React, {useEffect} from 'react';
+import { ThemeProvider } from 'styled-components/native';
 import {startDatabase} from '~/databases';
-import {Routes} from '~/routes';
+import { Routes } from '~/routes';
+import { lightTheme } from '~/themes';
 
 const App: React.FC = () => {
   useEffect(() => {
     startDatabase();
   }, []);
   return (
+    <ThemeProvider theme={lightTheme}>
     <NavigationContainer>
       <Routes />
     </NavigationContainer>
+    </ThemeProvider>
   );
 };
 
