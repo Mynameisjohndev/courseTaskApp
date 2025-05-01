93276459fc711f0c0942d3143e950c2e9a643428
Mynameisjohndev <joaoantonio15161516@gmail.com>
2025-04-30 23:26:28 -0300

    feat: create functionality to search for tasks and hide the create button during the process

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,21 +1,46 @@
-import {FC, useState} from 'react';
+import {FC, useEffect, useState} from 'react';
 import {AppScreenProps} from '~/types/navigation';
 import {FloatingButton, Header, HomeBackground, HomeTitle} from './styles';
 import {useUserContext} from '~/context/userContext';
 import {Content, SubTitleT1} from '~/global/styles';
 import {Button} from '~/components/Button';
 import Ilustration from '~/assets/svgs/ilustrations/ilustration-home.svg';
-import {FlatList} from 'react-native';
+import {FlatList, Keyboard} from 'react-native';
 import {SwitchAppTheme} from '~/components/SwitchAppTheme';
 import {Search} from '~/components/Search';
 import {ListCardTask} from '~/components/ListCardTask';
 import {UpsertTaskModal} from '~/components/UpsertTaskModal';
 import Plus from '~/assets/svgs/icons/plus.svg';
+import {Task} from '~/types/task';
 
 const Home: FC<AppScreenProps<'home'>> = ({}) => {
   const {user, tasks, openAndCloseModal, openModal} = useUserContext();
 
   const [search, setSearch] = useState<string>('');
+  const [filteredTasks, setFilteredTasks] = useState<Task[]>(tasks);
+  const [keyboardOpen, setkeyboardOpen] = useState<boolean>(false);
+
+  useEffect(() => {
+    const filtered = tasks.filter(task => {
+      const lowerCaseSearch = search.toLowerCase();
+      if (search === '') {
+        return true;
+      }
+      return (
+        task.title.toLowerCase().includes(lowerCaseSearch) ||
+        task.description.toLowerCase().includes(lowerCaseSearch)
+      );
+    });
+    setFilteredTasks(filtered);
+  }, [search, tasks]);
+
+  useEffect(()=>{
+    const listeners = [
+      Keyboard.addListener('keyboardDidShow', () => setkeyboardOpen(true)),
+      Keyboard.addListener('keyboardDidHide', () => setkeyboardOpen(false)),
+    ];
+    return () => listeners.forEach(l => l.remove());
+  },[]);
 
   return (
     <HomeBackground>
@@ -28,15 +53,17 @@ const Home: FC<AppScreenProps<'home'>> = ({}) => {
           <Search value={search} onChangeText={setSearch} />
           <FlatList
             showsVerticalScrollIndicator={false}
-            data={tasks}
+            data={filteredTasks}
             keyExtractor={item => String(item.id)}
             renderItem={({item}) => {
               return <ListCardTask {...item} key={item.id} />;
             }}
           />
-          <FloatingButton onPress={() => openAndCloseModal()}>
-            <Plus width={45} height={45} />
-          </FloatingButton>
+          {!keyboardOpen && (
+            <FloatingButton onPress={() => openAndCloseModal()}>
+              <Plus width={45} height={45} />
+            </FloatingButton>
+          )}
         </>
       ) : (
         <>
